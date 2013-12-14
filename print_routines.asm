2d14abedfa6e83c4c531ad5e55a5d753e12369e4
Seth Yoder <seth.a.yoder@gmail.com>
2013-12-14 17:16:35 -0500

    Added register dumping and binary integer printing.

--- print_routines.asm
+++ print_routines.asm
@@ -1,95 +1,105 @@
+
 segment .data
-        nl      db      0xA
-        nl_len  equ     $-nl
-        
+        nl:      db      0xA
+        nl_len:  equ     $-nl
+        reg_a:   db      "EAX: ",0
+        reg_b:   db      "EBX: ",0
+        reg_c:   db      "ECX: ",0
+        reg_d:   db      "EDX: ",0
+            
 segment .bss
-    to_print:   resb 1
-    data_save:  resd 1
+        to_print:   resb 1
+        data_save:  resd 1
 
 segment .text
-        global print_string
         global print_char_from_val
-        global print_char_from_ptr
-        global print_signed_int_val
         global print_nl
+        global print_char_from_ptr
+        global print_string
+        global print_signed_dec_int
+        global print_unsigned_bin_int
+        global dump_regs
 
 print_char_from_val:
-        push    dword ebx
-        push    dword ecx
-        push    dword edx
-        push    dword eax
+            push    dword ebx
+            push    dword ecx
+            push    dword edx
+            push    dword eax
 
-        mov     [to_print], eax
-        mov     eax, 4
-        mov     ebx, 1
-        mov     ecx, to_print
-        mov     edx, 1
-        int     0x80
+            mov     [to_print], eax
+            mov     eax, 4
+            mov     ebx, 1
+            mov     ecx, to_print
+            mov     edx, 1
+            int     0x80
 
-        pop     eax
-        pop     edx
-        pop     ecx
-        pop     ebx
-        
-        ret
-        
+            pop     eax
+            pop     edx
+            pop     ecx
+            pop     ebx
+            
+            ret
+            
 print_nl:
-        push    dword eax
-        push    dword ebx
-        push    dword ecx
-        push    dword edx
-        
-        mov     eax, 4
-        mov     ebx, 1
-        mov     ecx, nl
-        mov     edx, nl_len
-        int     0x80
+            push    dword eax
+            push    dword ebx
+            push    dword ecx
+            push    dword edx
+            
+            mov     eax, 4
+            mov     ebx, 1
+            mov     ecx, nl
+            mov     edx, nl_len
+            int     0x80
 
-        pop     edx
-        pop     ecx
-        pop     ebx
-        pop     eax
+            pop     edx
+            pop     ecx
+            pop     ebx
+            pop     eax
 
-        ret
-        
-;PRE: An ASCII character pointer will be given in eax
-;POST: The character will be printed to stdout.
+            ret
+            
+    ;PRE: An ASCII character pointer will be given in eax
+    ;POST: The character will be printed to stdout.
+    
 print_char_from_ptr:
-        push    dword ebx
-        push    dword ecx
-        push    dword edx
+            push    dword ebx
+            push    dword ecx
+            push    dword edx
 
-        ;eax is an int*
-        mov     ecx, eax    ;ecx = eax
-        mov     eax, 4      ;sys_write
-        mov     ebx, 1      ;to stdout (whatever is in ecx)
-        mov     edx, 1      ;1 byte
-        int     0x80        ;kernel, please?
+            ;eax is an int*
+            mov     ecx, eax    ;ecx = eax
+            mov     eax, 4      ;sys_write
+            mov     ebx, 1      ;to stdout (whatever is in ecx)
+            mov     edx, 1      ;1 byte
+            int     0x80        ;kernel, please?
 
-        mov     eax, ecx    ;eax = ecx
-        pop     edx         ;restore everything we saved on the stack
-        pop     ecx
-        pop     ebx
+            mov     eax, ecx    ;eax = ecx
+            pop     edx         ;restore everything we saved on the stack
+            pop     ecx
+            pop     ebx
 
-        ret                 ;return
-        
+            ret                 ;return
+            
 
 ;PRE: An ASCII string pointer will be given in eax
 ;POST: Will print the string to stdout.
 print_string:
+                push    dword eax
                 push    dword ebx   ;save whatever is in ebx
                 
         l1:     call    print_char_from_ptr  ;cout<<*eax;
-                add     eax, 1      ;eax++
-                mov     ebx, [eax]  ;ebx = *eax
-                cmp     ebx, 0      ;if (ebx != 0) {  //Strings are null-terminated!
+                add     eax, 1       ;eax++
+                mov     bl, [eax]   ;ebx = eax
+                cmp     bl, 0       ;if (ebx != 0) {  //Strings are null-terminated!
                 jne     l1          ;   goto label
                                     ;}
                 pop     ebx         ;restore ebx
+                pop     eax
 
                 ret                 ;return
 
-print_signed_int_val:
+print_signed_dec_int:
                 push    dword edx
                 push    dword ecx
                 push    dword ebx
@@ -127,4 +137,84 @@ print_signed_int_val:
                 pop     ecx
                 pop     edx
 
+                ret
+
+print_unsigned_bin_int:
+                push    dword eax
+                push    dword ebx
+                push    dword ecx
+                push    dword edx
+                mov     ebx, 31
+                
+  binprntloop:  rol     eax, 1
+                jc      print_1
+
+   print_0:     push    eax
+                mov     eax, 48
+                call    print_char_from_val
+                pop     eax
+                jmp     endprintbin
+   
+   print_1:     push    eax
+                mov     eax, 49
+                call    print_char_from_val
+                pop     eax
+                jmp     endprintbin
+
+   endprintbin: push    eax
+                mov     eax, ebx
+                mov     ecx, 8
+                cdq
+                div     ecx
+                cmp     edx,0
+                jne     noprintspace
+                mov     eax,32
+                call    print_char_from_val
+                
+  noprintspace: pop     eax
+                dec     ebx
+                cmp     ebx,0
+                jge     binprntloop
+
+                pop     edx
+                pop     ecx
+                pop     ebx
+                pop     eax
+
+                ret
+
+dump_regs:
+                push    eax
+                
+                push    eax
+                mov     eax,reg_a
+                call    print_string
+                pop     eax
+                call    print_unsigned_bin_int
+
+                call    print_nl
+                
+                mov     eax,reg_b
+                call    print_string
+                mov     eax,ebx
+                call    print_unsigned_bin_int
+
+                call    print_nl
+
+                mov     eax,reg_c
+                call    print_string
+                mov     eax,ecx
+                call    print_unsigned_bin_int
+                
+                call    print_nl
+
+                mov     eax,reg_a
+                call    print_string
+                mov     eax,edx
+                call    print_unsigned_bin_int
+                
+                call    print_nl
+
+                pop     eax
+                
                 ret
